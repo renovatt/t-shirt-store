4fa1a6d06d7b267572376c7df0874919d59c4f4d
will <wlymes@gmail.com>
2024-01-03 13:08:21 -0300

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Header from './components/the-header.vue';
 </script>
 
 <template>
+  <Header />
   <RouterView />
 </template>
 
---
+++ src/components/the-header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="sticky inset-0 flex w-full flex-col items-center justify-between gap-4 space-y-4 bg-900 p-2">
+    <section class="flex w-full items-center justify-between">
+      <img alt="Vue logo" src="@/assets/logo.svg" width="50" height="50" />
+      <input type="search" placeholder="Anime" name="" id="" class="w-96 p-2 px-4">
+
+      <section class="flex gap-2">
+        <span>user</span>
+        <span>cart</span>
+      </section>
+    </section>
+
+    <nav class="flex w-full items-center justify-center py-4">
+      <ul class="flex w-full items-center justify-between gap-4">
+        <li class="rounded-md p-2 px-4 text-center uppercase">categoria</li>
+        <li class="rounded-md p-2 px-4 text-center uppercase">categoria</li>
+        <li class="rounded-md p-2 px-4 text-center uppercase">categoria</li>
+        <li class="rounded-md p-2 px-4 text-center uppercase">categoria</li>
+        <li class="rounded-md p-2 px-4 text-center uppercase">categoria</li>
+        <li class="rounded-md p-2 px-4 text-center uppercase">categoria</li>
+        <li class="rounded-md p-2 px-4 text-center uppercase">categoria</li>
+      </ul>
+    </nav>
+  </header>
+</template>
