9fc9fac363c5f3236b82245e9121392c2b96594d
will <wlymes@gmail.com>
2024-01-04 11:14:57 -0300

    feat: filters section

---
+++ src/components/the-filters.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-2 flex items-center justify-start gap-2">
+    <button class="bg-700 p-2 px-4 text-base uppercase text-800">Filtrar</button>
+
+    <select class="border border-900 p-2 px-4 text-700/80 outline-none">
+      <option value="">Mais relevantes</option>
+      <option value="">Menor preço</option>
+      <option value="">Maior preço</option>
+      <option value="">Alfabetica de A>Z</option>
+      <option value="">Alfabetica de Z>A</option>
+    </select>
+
+    <select class="border border-900 p-2 px-4 text-700/80 outline-none">
+      <option value="24">24</option>
+      <option value="34">34</option>
+      <option value="48">48</option>
+    </select>
+  </div>
+</template>
