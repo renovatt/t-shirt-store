7034213a6bc53524a1ae48865a723df256eed0be
will <wlymes@gmail.com>
2024-01-19 13:36:33 -0300

    feat: image updated

--- src/components/the-banner.vue
+++ src/components/the-banner.vue
@@ -3,7 +3,8 @@ import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 
 const banners = [
-  "https://cdn.dooca.store/292/files/banner-full-1600x450-camisa10dez-02.jpg?v=1703767653&webp=0",
+  // "https://cdn.dooca.store/292/files/banner-full-1600x450-camisa10dez-02.jpg?v=1703767653&webp=0",
+  "https://cdn.dooca.store/292/files/banner-full-1600x450-frases.jpg?v=1698229590&webp=0",
   "https://cdn.dooca.store/292/files/banner-full-1600x500-born-in3.jpg?v=1695381701&webp=0",
   "https://cdn.dooca.store/292/files/banner-full-1600x450-personalizada-1.png?v=1698060073&webp=0"
 ]
