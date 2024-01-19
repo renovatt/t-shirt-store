013b8604ec54d1087bbc74eb9c1a5f4675323251
will <wlymes@gmail.com>
2024-01-19 13:36:01 -0300

    feat: footer

---
+++ src/components/the-footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer
+    className="items-center flex-col gap-4 md:flex-row tracking-normal justify-around flex border-t border-900 p-2 py-4">
+    <section class="container flex w-full flex-col items-center justify-between gap-4 md:flex-row md:gap-0">
+      <section class="flex flex-col items-center justify-center gap-2 md:items-start md:gap-0">
+        <span class="text-center text-xs text-700/70">
+          Todos direitos de imagens reservados aos seus criadores
+        </span>
+        <span class="text-center text-xs text-700/70">
+          Criado apenas para estudos, não há nenhuma comercialização de produtos
+        </span>
+      </section>
+      <section class="flex items-center justify-center">
+        <span className="text-xs font-bold text-700/70">
+          &copy; 2023 Powered by
+        </span>
+        <a className="flex items-center ml-1 text-xs transition-all font-bold text-700/70 hover:text-700"
+          href="https://www.linkedin.com/in/renovatt" target="_blank">
+          renovatt
+        </a>
+      </section>
+    </section>
+  </footer>
+</template>
+
--- src/views/view-home.vue
+++ src/views/view-home.vue
@@ -4,6 +4,7 @@ import Filters from '@/components/the-filters.vue';
 import CollectionBanner from '@/components/the-collection-banner.vue';
 import Products from '@/components/the-products.vue';
 import Header from '@/components/the-header.vue';
+import Footer from '@/components/the-footer.vue';
 </script>
 
 <template>
@@ -15,5 +16,6 @@ import Header from '@/components/the-header.vue';
       <Filters />
       <Products />
     </article>
+    <Footer />
   </section>
 </template>
